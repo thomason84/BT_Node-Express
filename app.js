c45c248b844267b60ebab3333145252cc67fee3b
Steven <steven@vetfriends.com>
2019-04-24 14:41:06 -0400

    delete config

---
+++ app.js
@@ -0,0 +1,100 @@
+'use strict';
+
+var express = require('express');
+var cors = require('cors');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var flash = require('connect-flash');
+
+var routes = require('./routes/index');
+
+var app = express();
+
+
+
+var allowedOrigins = ['http://localhost:8500', 'http://vf2.vetfriends.com','https://vf2.vetfriends.com/catalog/billing-shippingBT-part1alt.cfm', null];
+
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+
+  exposedHeaders: ['Content-Length', 'X-Foo', 'X-Bar'],
+
+  credentials: true
+}));
+
+
+
+
+
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(favicon(path.join(__dirname, 'public/images', 'favicon.png')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(session({
+  // this string is not an appropriate value for a production environment
+  // read the express-session documentation for details
+  secret: '---',
+  saveUninitialized: true,
+  resave: true
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(flash());
+
+
+
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function (err, req, res, next) { // eslint-disable-line no-unused-vars
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) { // eslint-disable-line no-unused-vars
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+module.exports = app;
