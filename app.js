83888e30559046419aee1ba5c40b871864439d54
Steven <steven@vetfriends.com>
2019-04-25 12:12:38 -0400

    removed express-session

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 var flash = require('connect-flash');
 
 var routes = require('./routes/index');
@@ -53,8 +54,6 @@ app.use(favicon(path.join(__dirname, 'public/images', 'favicon.png')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-
-
 app.use(cookieParser());
 app.use(session({
   // this string is not an appropriate value for a production environment
