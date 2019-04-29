b4ef666c8d48296fe5e97d50d2332c39389697e7
Steven <steven@vetfriends.com>
2019-04-29 10:25:37 -0400

    added jQuery

--- routes/index.js
+++ routes/index.js
@@ -110,7 +110,7 @@ router.post('/checkouts', function (req, res) {
         bodyFormData.set('amount', amount);
            return axios({
         method: 'post',
-        url: 'https://vf2.vetfriends.com/catalog/amtDC.cfm',
+        url: 'https://vf2.vetfriends.com/catalog/amtDS.cfm',
         data: bodyFormData,
         config: { headers: {'Content-Type': 'multipart/form-data' }}
         })
